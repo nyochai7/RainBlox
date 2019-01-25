a83dbb7cf7eb5a6ddd555967b605044b2ad17f73
Adar Grof <adar@mixed.place>
2019-01-25 14:35:49 +0200

    [MainMenu] added a main menu prefab

---
+++ Assets/Scripts/RootButton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RootButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler
+{
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        foreach(Selectable selectableChild in gameObject.GetComponentsInChildren<Selectable>())
+        {
+            PointerEventData newEventData = eventData;
+
+            newEventData.position = selectableChild.transform.position;
+            selectableChild.OnPointerUp(newEventData);
+        }      
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        foreach (Selectable selectableChild in gameObject.GetComponentsInChildren<Selectable>())
+        {
+            PointerEventData newEventData = eventData;
+
+            newEventData.position = selectableChild.transform.position;
+            selectableChild.OnPointerDown(newEventData);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (Selectable selectableChild in gameObject.GetComponentsInChildren<Selectable>())
+        {
+            PointerEventData newEventData = eventData;
+
+            newEventData.position = selectableChild.transform.position;
+            selectableChild.OnPointerExit(newEventData);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach (Selectable selectableChild in gameObject.GetComponentsInChildren<Selectable>())
+        {
+            PointerEventData newEventData = eventData;
+
+            newEventData.position = selectableChild.transform.position;
+            selectableChild.OnPointerEnter(newEventData);
+        }
+    }
+}
